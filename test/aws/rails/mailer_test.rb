b8e971f67f3981e59685b30b602665a1abd1f506
Jonathan Hefner <jonathan@hefner.pro>
2019-10-09 14:24:06 -0500

    Test Aws::Rails::Railtie

--- test/aws/rails/mailer_test.rb
+++ test/aws/rails/mailer_test.rb
@@ -1,6 +1,20 @@
 require 'test_helper'
 require 'mail'
 
+class TestMailer < ActionMailer::Base
+  layout nil
+
+  def deliverable(options = {})
+    mail(
+      body: options[:body],
+      delivery_method: :aws_sdk,
+      from: options[:from],
+      subject: options[:subject],
+      to: options[:to]
+    )
+  end
+end
+
 module Aws
   module Rails
     class MailerTest < Minitest::Test
---
+++ test/dummy/app/assets/config/manifest.js
@@ -0,0 +1,1 @@
+// This file exists to appease Sprockets 4
