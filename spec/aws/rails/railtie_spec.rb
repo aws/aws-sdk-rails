b2a84a26a1a980a5b3438967c4e54bf702530518
Alex Woods <alexwoo@amazon.com>
2020-11-05 15:43:51 -0800

    Remove commented out tests

--- spec/aws/rails/railtie_spec.rb
+++ spec/aws/rails/railtie_spec.rb
@@ -28,25 +28,5 @@ module Aws
       end
 
     end
-    #
-    # class RailtieTest < Minitest::Test
-    #   def test_add_action_mailer_delivery_method
-    #     assert_equal ::Aws::Rails::Mailer,
-    #                  ::ActionMailer::Base.delivery_methods[:ses]
-    #   end
-    #
-    #   def test_log_to_rails_logger
-    #     assert_equal ::Rails.logger, Aws.config[:logger]
-    #   end
-    #
-    #   def test_use_rails_encrypted_credentials
-    #     creds = ::Rails.application.credentials.aws
-    #     assert_equal Aws.config[:access_key_id], creds[:access_key_id]
-    #     assert_equal Aws.config[:secret_access_key], creds[:secret_access_key]
-    #
-    #     refute_nil creds[:non_credential_key]
-    #     assert_nil Aws.config[:non_credential_key]
-    #   end
-    # end
   end
 end
