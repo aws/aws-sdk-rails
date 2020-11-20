2dc8da822a9cf297bbec030b46f86a4a5843ba7f
Alex Woods <alexwoo@amazon.com>
2020-11-20 14:47:34 -0800

    Various cleanups

--- lib/active_job/queue_adapters/aws_sqs_adapter.rb
+++ lib/active_job/queue_adapters/aws_sqs_adapter.rb
@@ -30,52 +30,9 @@ module ActiveJob
       def _enqueue(job, send_message_opts = {})
         body = job.serialize
         queue_url = Aws::Rails::SqsActiveJob.config.queue_url_for(job.queue_name)
-        puts "#{job.queue_name} => #{queue_url}"
         send_message_opts[:queue_url] = queue_url
         send_message_opts[:message_body] = Aws::Json.dump(body)
         @client.send_message(send_message_opts)
-      end
-
-
-      class Scheduler #:nodoc:
-        DEFAULT_EXECUTOR_OPTIONS = {
-                                     min_threads:     0,
-                                     max_threads:     Concurrent.processor_count,
-                                     auto_terminate:  true,
-                                     idletime:        60, # 1 minute
-                                     max_queue:       0, # unlimited
-                                     fallback_policy: :caller_runs # shouldn't matter -- 0 max queue
-                                   }.freeze
-
-        attr_accessor :immediate
-
-        def initialize(**options)
-          self.immediate = false
-          @immediate_executor = Concurrent::ImmediateExecutor.new
-          @async_executor = Concurrent::ThreadPoolExecutor.new(DEFAULT_EXECUTOR_OPTIONS.merge(options))
-        end
-
-        def enqueue(job, queue_name:)
-          executor.post(job, &:perform)
-        end
-
-        def enqueue_at(job, timestamp, queue_name:)
-          delay = timestamp - Time.current.to_f
-          if delay > 0
-            Concurrent::ScheduledTask.execute(delay, args: [job], executor: executor, &:perform)
-          else
-            enqueue(job, queue_name: queue_name)
-          end
-        end
-
-        def shutdown(wait: true)
-          @async_executor.shutdown
-          @async_executor.wait_for_termination if wait
-        end
-
-        def executor
-          immediate ? @immediate_executor : @async_executor
-        end
       end
     end
   end
