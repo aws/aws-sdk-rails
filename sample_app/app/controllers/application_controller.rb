e50668757dbfd97915460449e371eef67ebb99b6
Alex Woods <alexwoo@amazon.com>
2020-12-10 15:20:28 -0800

    Add a demo route for running a test job.

--- sample_app/app/controllers/application_controller.rb
+++ sample_app/app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+  def test_job
+    job = HelloJob.perform_later(params[:name])
+    Rails.logger.info "Queued up a HelloJob: #{job}"
+    render json: {job: job}
+  end
 end
--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/test-job', to: 'application#test_job'
 end
