c29890e123ea51ff27344a555c18b0104cc80534
Alex Woods <alexwoo@amazon.com>
2022-01-20 10:57:55 -0800

    Undo changes to sample app

--- sample_app/config/application.rb
+++ sample_app/config/application.rb
@@ -1,9 +1,6 @@
 require_relative 'boot'
 
-require 'rails'
-require 'active_job/railtie'
-require 'action_controller/railtie'
-require 'action_mailer/railtie'
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
