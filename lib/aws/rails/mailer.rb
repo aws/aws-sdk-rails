c8ba9f3976e7ad8157ce3fc71c6f607d68d40fe5
Matt Muller <mamuller@amazon.com>
2022-12-20 17:05:09 -0500

    Add shim to new mailer file (old one was not required anymore)

--- lib/aws/rails/mailer.rb
+++
@@ -1,3 +0,0 @@
-# This is for backwards compatibility after introducing support for SESv2.
-# The old mailer is now replaced with the new SES (v1) mailer.
-Aws::Rails::Mailer = Aws::Rails::SesMailer
--- lib/aws/rails/ses_mailer.rb
+++ lib/aws/rails/ses_mailer.rb
@@ -38,3 +38,7 @@ module Aws
     end
   end
 end
+
+# This is for backwards compatibility after introducing support for SESv2.
+# The old mailer is now replaced with the new SES (v1) mailer.
+Aws::Rails::Mailer = Aws::Rails::SesMailer
