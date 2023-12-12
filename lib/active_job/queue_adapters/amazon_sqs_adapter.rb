ca30aecc74fed4464dfb43ce709c942157801faf
Keito Kira <keito.kira@kyto64.com>
2023-12-12 17:55:47 +0900

    Fix negative delay.
    Issue: https://github.com/aws/aws-sdk-rails/issues/106

--- lib/active_job/queue_adapters/amazon_sqs_adapter.rb
+++ lib/active_job/queue_adapters/amazon_sqs_adapter.rb
@@ -11,6 +11,8 @@ module ActiveJob
 
       def enqueue_at(job, timestamp)
         delay = (timestamp - Time.now.to_f).floor
+
+        delay = 0 if delay.negative?
         raise ArgumentError, 'Unable to queue a job with a delay great than 15 minutes' if delay > 15.minutes
 
         _enqueue(job, nil, delay_seconds: delay)
