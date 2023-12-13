debec325ef1d79e60320ee1e24fd0f04f9415712
Keito Kira <keito.kira@kyto64.com>
2023-12-13 21:46:46 +0900

    Fix negative DelaySeconds being passed to parameter. (#107)

--- test/active_job/queue_adapters/amazon_sqs_adapter_test.rb
+++ test/active_job/queue_adapters/amazon_sqs_adapter_test.rb
@@ -144,29 +144,49 @@ module ActiveJob
         end
       end
 
-      it 'enqueues delayed jobs' do
-        t1 = Time.now
-        allow(Time).to receive(:now).and_return t1
+      describe 'with queue delay' do
+        it 'enqueues jobs with proper delay' do
+          t1 = Time.now
+          allow(Time).to receive(:now).and_return t1
 
-        expect(client).to receive(:send_message).with(
-          {
-            queue_url: 'https://queue-url',
-            delay_seconds: 60,
-            message_body: instance_of(String),
-            message_attributes: instance_of(Hash)
-          }
-        )
+          expect(client).to receive(:send_message).with(
+            {
+              queue_url: 'https://queue-url',
+              delay_seconds: 60,
+              message_body: instance_of(String),
+              message_attributes: instance_of(Hash)
+            }
+          )
 
-        TestJob.set(wait: 1.minute).perform_later('test')
-        sleep(0.2)
-      end
+          TestJob.set(wait: 1.minute).perform_later('test')
+          sleep(0.2)
+        end
 
-      it 'raises an error when job delay is great than SQS support' do
-        t1 = Time.now
-        allow(Time).to receive(:now).and_return t1
-        expect do
-          TestJob.set(wait: 1.day).perform_later('test')
-        end.to raise_error ArgumentError
+        it 'enqueues jobs with zero or negative delay' do
+          t1 = Time.now
+          allow(Time).to receive(:now).and_return t1
+
+          expect(client).to receive(:send_message).with(
+            {
+              queue_url: 'https://queue-url',
+              delay_seconds: 0,
+              message_body: instance_of(String),
+              message_attributes: instance_of(Hash)
+            }
+          ).twice
+
+          TestJob.set(wait: 0).perform_later('test')
+          TestJob.set(wait: -1).perform_later('test')
+          sleep(0.2)
+        end
+
+        it 'raises an error when job delay is great than SQS support' do
+          t1 = Time.now
+          allow(Time).to receive(:now).and_return t1
+          expect do
+            TestJob.set(wait: 1.day).perform_later('test')
+          end.to raise_error ArgumentError
+        end
       end
     end
   end
