41fc965d2f743ec94cdf08157b9b00c9bee17932
Sarah Sunday <3252602+ssunday@users.noreply.github.com>
2024-07-03 08:58:17 -0500

    [TEST] Tweak stubbing flow to maybe fix tests

--- spec/aws/rails/action_mailbox/requests/inbound_email_spec.rb
+++ spec/aws/rails/action_mailbox/requests/inbound_email_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'test_helper'
+require 'aws-sdk-s3'
 
 describe 'inbound email', type: :request do
   let(:inbound_email_url) { '/rails/action_mailbox/amazon/inbound_emails' }
@@ -22,12 +23,11 @@ describe 'inbound email', type: :request do
   it 'receives an inbound email with data in s3' do
     s3_email = fixture_for(:s3_email, type: :txt)
 
-    Aws.config[:s3] = {
-      stub_responses: {
-        head_object: { content_length: s3_email.size, parts_count: 1 },
-        get_object: { body: s3_email }
-      }
-    }
+    s3_client = Aws::S3::Client.new(stub_responses: true)
+    s3_client.stub_responses(:head_object, { content_length: s3_email.size, parts_count: 1 })
+    s3_client.stub_responses(:get_object, { body: s3_email })
+
+    allow(Aws::S3::Client).to receive(:new).and_return(s3_client)
 
     expect do
       post inbound_email_url,
