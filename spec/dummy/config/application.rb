f76560c56289fea1bb63bcc9a733727ae3281ba6
Sarah Sunday <3252602+ssunday@users.noreply.github.com>
2024-07-08 12:58:45 -0500

    [TEST] Use test queue adapter

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -15,6 +15,6 @@ module Dummy
     config.eager_load = false
     config.require_master_key = true
 
-    config.active_job.queue_adapter = :sqs
+    config.active_job.queue_adapter = :test
   end
 end
