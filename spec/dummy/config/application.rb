86e955a7960bd882ab24594b943d0a305ea5de47
Matt Muller <mamuller@amazon.com>
2024-07-08 09:57:27 -0400

    Try removing require activerecord adapter

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'rails'
-require 'activerecord-jdbc-adapter' if defined? JRUBY_VERSION
 require 'active_job/railtie'
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
