840b9c2630c0e7491a4b04e5354e16445d3f855a
Matt Muller <mamuller@amazon.com>
2024-07-21 20:56:10 -0400

    Set action mailbox config inside initializer

--- lib/aws/rails/action_mailbox/engine.rb
+++ lib/aws/rails/action_mailbox/engine.rb
@@ -1,16 +1,14 @@
 # frozen_string_literal: true
 
-require 'action_mailbox/engine'
-
 module Aws
   module Rails
     module ActionMailbox
       # @api private
       class Engine < ::Rails::Engine
-        config.action_mailbox.ses = ActiveSupport::OrderedOptions.new
-        config.action_mailbox.ses.s3_client_options ||= {}
-
         initializer 'aws-sdk-rails.mount_engine' do |app|
+          app.config.action_mailbox.ses = ActiveSupport::OrderedOptions.new
+          app.config.action_mailbox.ses.s3_client_options ||= {}
+
           app.routes.append do
             mount Engine => '/'
           end
