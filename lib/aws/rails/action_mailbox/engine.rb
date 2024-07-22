d9ae33a0200b0f42b1b9d1b9a322ef7e927dd836
Matt Muller <mamuller@amazon.com>
2024-07-22 17:19:44 -0400

    Check if action mailbox engine is defined instead

--- lib/aws/rails/action_mailbox/engine.rb
+++ lib/aws/rails/action_mailbox/engine.rb
@@ -5,14 +5,14 @@ module Aws
     module ActionMailbox
       # @api private
       class Engine < ::Rails::Engine
-        if config.respond_to?(:action_mailbox)
+        if defined?(ActionMailbox::Engine)
           config.action_mailbox.ses = ActiveSupport::OrderedOptions.new
           config.action_mailbox.ses.s3_client_options ||= {}
-        end
 
-        initializer 'aws-sdk-rails.mount_engine' do |app|
-          app.routes.append do
-            mount Engine => '/'
+          initializer 'aws-sdk-rails.mount_engine' do |app|
+            app.routes.append do
+              mount Engine => '/'
+            end
           end
         end
       end
