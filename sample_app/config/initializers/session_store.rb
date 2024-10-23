e13b7598fa52377512535c3cee05398db60509a9
Matt Muller <mamuller@amazon.com>
2024-10-23 14:47:52 -0400

    Attempt to fix sample app CI again

--- sample_app/config/initializers/session_store.rb
+++ sample_app/config/initializers/session_store.rb
@@ -1,2 +1,2 @@
-options = { table_name: '_sample_app_session', key: 'should not be here' }
+options = { table_name: '_sample_app_sessions', key: '_sample_app_session' }
 Rails.application.config.session_store :dynamo_db_store, **options
