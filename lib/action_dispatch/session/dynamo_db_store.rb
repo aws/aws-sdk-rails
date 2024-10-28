cf5f2b3198e364742e695889a0dfa84008f35d78
Matt Muller <mamuller@amazon.com>
2024-10-28 14:09:06 -0400

    Use compatibility to fix Rails 7.2 usage

--- lib/action_dispatch/session/dynamo_db_store.rb
+++ lib/action_dispatch/session/dynamo_db_store.rb
@@ -17,10 +17,20 @@ module ActionDispatch
     #
     # @see https://docs.aws.amazon.com/sdk-for-ruby/aws-sessionstore-dynamodb/api/Aws/SessionStore/DynamoDB/Configuration.html
     class DynamoDbStore < Aws::SessionStore::DynamoDB::RackMiddleware
+      # Because of how Ruby searches for methods in the inheritance chain,
+      # generate_sid in Compatibility takes precedence over our generate_sid.
+      # Compatibility is needed so that the request is an ActionDispatch::Request.
+      CompatibilityWithoutSid = Compatibility.dup
+      CompatibilityWithoutSid.remove_method(:generate_sid)
+      include CompatibilityWithoutSid
+
       include StaleSessionCheck
       include SessionObject
 
       def initialize(app, options = {})
+        Rails.logger.warn('** aws-sessionstore-dynamodb will no longer be a direct dependency of aws-sdk-rails ~> 5. ' \
+                          'To avoid disruption, please add aws-sessionstore-dynamodb ~> 2 to your Gemfile to enable '\
+                          'this feature when upgrading to aws-sdk-rails ~> 5. **')
         options[:config_file] ||= config_file
         options[:secret_key] ||= Rails.application.secret_key_base
         super
@@ -38,9 +48,9 @@ module ActionDispatch
     # @api private
     class DynamodbStore < DynamoDbStore
       def initialize(app, options = {})
-        Rails.logger.warn('Session Store :dynamodb_store has been renamed to :dynamo_db_store, ' \
-                          'please use the new key instead. :dynamodb_store will be removed in ' \
-                          'aws-sdk-rails ~> 5')
+        Rails.logger.warn('** Session Store :dynamodb_store configuration key has been renamed to :dynamo_db_store, ' \
+                          'please use the new key instead. The :dynamodb_store key name will be removed in ' \
+                          'aws-sdk-rails ~> 5 **')
         super
       end
     end
--- sample_app/config/initializers/session_store.rb
+++ sample_app/config/initializers/session_store.rb
@@ -1,2 +1,2 @@
-options = { table_name: '_sample_app_sessions', key: '_sample_app_session' }
+options = { key: '_sample_app_session' }
 Rails.application.config.session_store :dynamo_db_store, **options
