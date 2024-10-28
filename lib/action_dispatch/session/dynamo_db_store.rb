499e7f2cd8a44098fb332b12f1fbe8cfd7b6ba4e
Matt Muller <mamuller@amazon.com>
2024-10-28 14:32:50 -0400

    Bump aws-sessionstore-dynamodb gem dependency to ~> 3

--- lib/action_dispatch/session/dynamo_db_store.rb
+++ lib/action_dispatch/session/dynamo_db_store.rb
@@ -19,7 +19,7 @@ module ActionDispatch
     class DynamoDbStore < ActionDispatch::Session::AbstractStore
       def initialize(app, options = {})
         Rails.logger.warn('** aws-sessionstore-dynamodb will no longer be a direct dependency of aws-sdk-rails ~> 5. ' \
-                          'To avoid disruption, please add aws-sessionstore-dynamodb ~> 2 to your Gemfile to enable ' \
+                          'To avoid disruption, please add aws-sessionstore-dynamodb ~> 3 to your Gemfile to enable ' \
                           'this feature when upgrading to aws-sdk-rails ~> 5. **')
         options[:config_file] ||= config_file
         options[:secret_key] ||= Rails.application.secret_key_base
